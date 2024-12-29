952d2cb1fe402e3935999f1f446a4b770175a3d4
Filippo Spiga <spiga.filippo@gmail.com>
2024-12-28 23:35:46 -0800

    Added a GPU virtual package to control CUDA hierarchy in lmod modulefile generation

--- var/spack/repos/builtin/packages/cuda/package.py
+++ var/spack/repos/builtin/packages/cuda/package.py
@@ -699,6 +699,8 @@ class Cuda(Package):
     # see https://docs.nvidia.com/cuda/archive/11.4.0/cuda-gdb/index.html#release-notes
     depends_on("ncurses abi=5", type="run", when="@:11.3.99+dev")
 
+    provides("gpu")
+
     provides("opencl@:1.2", when="@7:")
     provides("opencl@:1.1", when="@:6")
 
---
+++ var/spack/repos/builtin/packages/gpu/package.py
@@ -0,0 +1,15 @@
+# Copyright 2013-2024 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+import os
+
+from spack.package import *
+
+
+class Gpu(Package):
+    """Virtual package for CUDA."""
+
+    homepage = "https://www.nvidia.com/"
+    virtual = True
